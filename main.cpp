b088bf3049c0397f9c48a35d03d40f25c7bee3cc
MadeInLithuania <tadulisss2012@gmail.com>
2026-04-22 17:28:54 +0200

    Pre merge commit

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@
 int main() {
     auto *nav = new Navigation();
     auto *rice = new Rice(0,"",{},0);
-    rice->CheckForDatabase();
     nav->GetHomeDir();
     nav->DisplayMenu();
     return SUCCESS;
